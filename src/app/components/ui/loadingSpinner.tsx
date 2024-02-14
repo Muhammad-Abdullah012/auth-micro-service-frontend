57a1fe23f2515a58a61ecbcb36d55ab6648a45b6
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-02-14 15:40:16 +0500

    Added loading spinner

---
+++ src/app/components/ui/loadingSpinner.tsx
@@ -0,0 +1,7 @@
+export const LoadingSpinner = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center self-stretch">
+      <div id="loading-spinner"></div>
+    </div>
+  );
+};
