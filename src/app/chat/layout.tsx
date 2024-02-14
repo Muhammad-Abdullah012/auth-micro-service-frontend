f1174c17766413611152bac252e5be6d6fd4709e
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-02-14 15:39:18 +0500

    Implemented auth guard

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,11 +1,16 @@
 import { Header } from "@/components/ui/header";
 import React from "react";
 import { ChatContextProvider } from "./chatContext";
+import { AuthGuard } from "@/components/auth/authGuard";
 const chatLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="min-h-screen">
-      <Header />
-      <ChatContextProvider>{children}</ChatContextProvider>
+      <ChatContextProvider>
+        <AuthGuard>
+          <Header />
+          {children}
+        </AuthGuard>
+      </ChatContextProvider>
     </main>
   );
 };
